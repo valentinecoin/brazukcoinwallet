806eadc1c09bf3639b3054924cb87b979fc6c10a
valentinecoin <36106470+valentinecoin@users.noreply.github.com>
2018-02-12 15:29:00 +0100

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,5 +1,5 @@
 
-set(CN_PROJECT_NAME "Brazukcoin")
-set(CN_CURRENCY_DISPLAY_NAME "Karbo")
-set(CN_CURRENCY_TICKER "BZK")
+set(CN_PROJECT_NAME "Valentinecoin")
+set(CN_CURRENCY_DISPLAY_NAME "Valentine")
+set(CN_CURRENCY_TICKER "VAL")
 set(CN_VERSION 1.2.1)
